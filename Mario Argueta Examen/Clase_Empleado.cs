60018845f4ea03bb97097925d4df0d7245e4f0b4
mario <mario@LAPTOP-5F5FBU6G>
2019-06-10 13:30:07 -0600

    Cambios en la clase Empleado

---
+++ Mario Argueta Examen/Clase_Empleado.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+namespace Mario_Argueta_Examen
+{
+    class Clase_Empleado : INotifyPropertyChanged
+    {
+        public string nombre;
+        public string identidad;
+        public string horasTrabajadas;
+        public string pagoPorHora;
+        public string horaExtra;
+        public string iHSS;
+        public string rAP;
+        public string sueldoSinDeducciones;
+        public string deducciones;
+        public string sueltoTotal;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+        public string Identidad
+        {
+            get
+            {
+                return identidad;
+            }
+            set
+            {
+                identidad = value;
+            }
+        }
+        public string HorasTrabajadas
+        {
+            get { return horasTrabajadas; }
+            set
+            {
+                int num;
+                bool sueldoSinDeduccion = int.TryParse(value, out num);
+                if (sueldoSinDeduccion) horasTrabajadas = value;
+                OnPropertyChanged("HorasTrabajadas");
+                OnPropertyChanged("SueldoSinDeducciones");
+            }
+        }
+        public string PagoPorHora
+        {
+            get { return pagoPorHora; }
+            set
+            {
+                int num;
+                bool sueldoSinDeduccion = int.TryParse(value, out num);
+                if (sueldoSinDeduccion) pagoPorHora = value;
+                OnPropertyChanged("PagoPorHora");
+                OnPropertyChanged("SueldoSinDeducciones");
+            }
+        }
+        public string SueldoSinDeducciones
+        {
+            get
+            {
+                int sueldoSinDeducciones = int.Parse(HorasTrabajadas) * int.Parse(PagoPorHora);
+                return sueldoSinDeducciones.ToString();
+            }
+            set
+            {
+                int multi = int.Parse(HorasTrabajadas) * int.Parse(PagoPorHora);
+                SueldoSinDeducciones = multi.ToString();
+            }
+        }
+        public string IHSS
+        {
+            get
+            {
+                int IHSS = int.Parse(SueldoSinDeducciones) * int.Parse("0.04");
+                return IHSS.ToString();
+            }
+            set
+            {
+                int mult = int.Parse(SueldoSinDeducciones) * int.Parse("0.04");
+                IHSS = mult.ToString();
+            }
+        }
+
+    }
+}
