13e8357bb6280a97970e759e409b035b37347ccb
mario <mario@LAPTOP-5F5FBU6G>
2019-06-10 13:09:28 -0600

    Cambios en la clase

---
+++ Mario Argueta Examen/Clase_Calculos.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+namespace Mario_Argueta_Examen
+{
+    class Clase_Calculos : INotifyPropertyChanged
+    {
+        public string nombre;
+        public string identidad;
+        public string horasTrabajadas;
+        public string pagoPorHora;
+        public string horaExtra;
+        public string iHSS;
+        public string rAP;
+        public string sueldoSinDeducciones;
+        public string deducciones;
+        public string sueltoTotal;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+        public string Identidad
+        {
+            get
+            {
+                return identidad;
+            }
+            set
+            {
+                identidad = value;
+            }
+        }
+        public string HorasTrabajadas
+        {
+            get { return horasTrabajadas; }
+            set
+            {
+                int num;
+                bool sueldoSinDeduccion
+            }
+        }
+    }
+}
